14f100a9b8ad686400cb0d0e1375ff74b274038d
Izaart <izabelle.sdn@gmail.com>
2022-04-25 19:21:25 -0300

    ícone contraste

--- js/evento.js
+++ js/evento.js
@@ -5,18 +5,8 @@ const rodape = document.querySelector("footer");
 // Evento de click para ativar e desativar
 btn.addEventListener("click", function(){
     pagina.classList.toggle("modo-contraste");
-    if(btn.textContent == 'Ativar Contraste') {
-        btn.textContent = 'Desativar Contraste';
-    } else {
-        btn.textContent = 'Ativar Contraste';
-    }
 });
 
 btn.addEventListener("click", function(){
-    rodape.classList.toggle("rodape-contraste");
-    if(btn.textContent == 'Ativar Contraste') {
-        btn.textContent = 'Desativar Contraste';
-    } else {
-        btn.textContent = 'Ativar Contraste';
-    }
+    rodape.classList.toggle("rodape-contraste"); 
 });
