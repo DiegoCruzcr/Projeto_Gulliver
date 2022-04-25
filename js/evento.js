7f629ce19944a87fd94b53e706d43c6c5b72806c
Izaart <izabelle.sdn@gmail.com>
2022-04-25 15:16:03 -0300

    acessibilidade

--- js/evento.js
+++ js/evento.js
@@ -1,13 +1,12 @@
-/* Manipulando o DOM */
 const btn = document.querySelector(".btn-pcd");
 const pagina = document.querySelector("body");
 
 // Evento de click para ativar e desativar
 btn.addEventListener("click", function(){
     pagina.classList.toggle("modo-contraste");
-    if(btn.textContent == 'Ativar') {
-        btn.textContent = 'Desativar';
+    if(btn.textContent == 'Ativar Contraste') {
+        btn.textContent = 'Desativar Contraste';
     } else {
-        btn.textContent = 'Ativar';
+        btn.textContent = 'Ativar Contraste';
     }
 });
--- js/letra.js
+++ js/letra.js
@@ -1,23 +1,23 @@
 window.onload = function() {
-    var elementBody = document.querySelector("body");
-    var elementBtnIncreaseFont = document.querySelector("#aumentar");
-    var elementBtnDecreaseFont = document.querySelector("#diminuir");
+    let pagina = document.querySelector("body");
+    let aumentarFonte = document.querySelector("#aumentar");
+    let diminuirFonte = document.querySelector("#diminuir");
     
     // Padrão de tamanho, equivale a 100% do valor definido
-    var fontSize = 100;
+    let fontSize = 100;
     
     // Valor de incremento ou decremento, equivale a 10% do valor da fonte
-    var increaseDecrease = 10;
+    let alternar = 10;
 
     // Evento de click para aumentar a fonte
-    elementBtnIncreaseFont.addEventListener("click", function(event) {
-        fontSize = fontSize + increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
+    aumentarFonte.addEventListener("click", function(event) {
+        fontSize = fontSize + alternar;
+        pagina.style.fontSize = fontSize + '%';
     });
 
     // Evento de click para diminuir a fonte
-    elementBtnDecreaseFont.addEventListener("click", function(event) {
-        fontSize = fontSize - increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
+    diminuirFonte.addEventListener("click", function(event) {
+        fontSize = fontSize - alternar;
+        pagina.style.fontSize = fontSize + '%';
     });
 }
