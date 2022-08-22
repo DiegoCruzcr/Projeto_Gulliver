b6159b5f8212d60317a35308695d4d47c2348de6
Diego Cruz <73648247+DiegoCruzcr@users.noreply.github.com>
2022-08-21 21:46:22 -0300

    Add files via upload

---
+++ gulliver-eclipse/gulliver/src/br/com/gulliver/beans/PontoTuristico.java
@@ -0,0 +1,40 @@
+package br.com.gulliver.beans;
+
+import java.util.ArrayList;
+
+public class PontoTuristico extends Usuario{
+	private int Id;
+	private String Descricao;
+	private ArrayList<String> EnderecoLista;
+	
+	public PontoTuristico() {
+		super();
+	}
+	
+	public PontoTuristico(int id, String descricao, ArrayList<String> endereco) {
+		super();
+		this.Id = id;
+		this.Descricao = descricao;
+		this.EnderecoLista = endereco;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getDescricao() {
+		return Descricao;
+	}
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+	public ArrayList<String> getEndereco() {
+		return EnderecoLista;
+	}
+	public void setEnderecoLista(ArrayList<String> endereco) {
+		EnderecoLista = endereco;
+	}
+	
+	
+}
