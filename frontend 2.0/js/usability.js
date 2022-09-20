9ddc16cf2e0a9d279d28d5d98e26e9e3374c4c32
Pedro Soares <pedrowesleyfs@gmail.com>
2022-09-19 21:33:00 -0300

    adicionado JS para botão hospedagem

---
+++ frontend 2.0/js/usability.js
@@ -0,0 +1,5 @@
+    <script>
+      document.getElementById("go-hospedagem").onclick = function() {
+        window.location.href = "http://127.0.0.1:5503/frontend%202.0/html/hospedagem.html"};
+      ;
+  </script>
